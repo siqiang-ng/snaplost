afb23c681d0211c74a7e2c828b115619f59ea135
sq125 <nsq.siqiang@gmail.com>
2020-06-15 01:11:22 +0800

    Added an edit page to edit the description

--- app.py
+++ app.py
@@ -34,4 +34,49 @@ def item(item_id):
 
 @app.route('/create', methods=('GET', 'POST'))
 def create():
+    if request.method == 'POST':
+        item = request.form['item']
+        description = request.form['description']
+        occurdate = request.form['occurdate']
+        time = request.form['time']
+        photo = request.form['photo']
+
+        if not item:
+            flash('Item name is required!')
+        if not description:
+            flash('Item description is required!')
+        if not photo:
+            flash('Photo of the item is required!')
+        else:
+            conn = get_db_connection()
+            conn.execute('INSERT INTO items (item, description, occurdate, time, photo) VALUES (?, ?, ?, ?, ?)',
+                    (item, description, occurdate, time, photo))
+            conn.commit()
+            conn.close()
     return render_template('create.html')
+
+@app.route('/<int:id>/edit', methods=('GET', 'POST'))
+def edit(id):
+    item = get_item(id)
+    
+    if request.method == 'POST':
+        item = request.form['item']
+        description = request.form['description']
+        occurdate = request.form['occurdate']
+        time = request.form['time']
+        photo = request.form['photo']
+
+        if not item:
+            flash('Item name is required!')
+        if not description:
+            flash('Item description is required!')
+        if not photo:
+            flash('Photo of the item is required!')
+        else:
+            conn = get_db_connection()
+            conn.execute('UPDATE items SET item = ?, description = ?, occurdate = ?, time = ?, photo =?' 
+                    'WHERE id = ?', (item, description, occurdate, time, photo, id))
+            conn.commit()
+            conn.close()
+    return render_template('edit.html', item=item)
+    
