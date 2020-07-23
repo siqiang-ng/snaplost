cd331551b0ccbfec1630b7edf4278cdb6ccdf923
sq125 <nsq.siqiang@gmail.com>
2020-07-24 00:42:26 +0800

    Move config to site-packages

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_mail import Mail
-from .config import Config
+from config import Config
 from elasticsearch import Elasticsearch
 import os
 
