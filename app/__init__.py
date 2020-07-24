aa989260829d1e2cbc398534413664c5c4c6a795
sq125 <nsq.siqiang@gmail.com>
2020-07-24 22:14:58 +0800

    Change elasticsearch config

--- app/__init__.py
+++ app/__init__.py
@@ -20,7 +20,7 @@ def create_app():
     db.init_app(app)
     migrate = Migrate(app, db, render_as_batch=True)
 
-    app.elasticsearch = Elasticsearch("http://localhost:9200") \
+    app.elasticsearch = Elasticsearch([app.config['ELASTICSEARCH_URL']]) \
         if app.config['ELASTICSEARCH_URL'] else None
 
     login_manager = LoginManager()
