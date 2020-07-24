c3f9d86cafa17001b4e17273722f3ab0b525e850
sq125 <nsq.siqiang@gmail.com>
2020-07-24 23:23:39 +0800

    Edit the config

--- config.py
+++ config.py
@@ -1,13 +1,17 @@
 import os 
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 class Config(object):
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
-	SECRET_KEY = os.urandom(24)
-	SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite'
+	SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+	# SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite'
+	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
 	MAIL_SERVER='smtp.gmail.com'
 	MAIL_PORT = 465
-	MAIL_USERNAME = 'esqlostesq@gmail.com'
-	MAIL_PASSWORD = 'snaplost'
+	MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+	MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
 	MAIL_USE_TLS = False
 	MAIL_USE_SSL = True
 	POSTS_PER_PAGE = 24
