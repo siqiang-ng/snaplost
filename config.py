de19558f5e2383efda354ab8fe2f9480b97020de
sq125 <nsq.siqiang@gmail.com>
2020-07-27 11:34:54 +0800

    Modify config.py

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ class Config(object):
 	POSTS_PER_PAGE = 24
 	UPLOAD_FOLDER = os.path.join(os.getcwd(), 'app/static')
 	ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
-	S3_BUCKET = os.environ.get("S3_BUCKET")
-	S3_KEY = os.environ.get("S3_KEY")
-	S3_SECRET = os.environ.get("S3_SECRET_ACCESS_KEY")
+	S3_BUCKET = os.environ.get("S3_BUCKET_NAME")
+	S3_KEY = os.environ.get("AWS_ACCESS_KEY_ID")
+	S3_SECRET = os.environ.get("AWS_SECRET_ACCESS_KEY")
 	S3_LOCATION = "https://{}.s3-ap-southeast-1.amazonaws.com/".format(S3_BUCKET)
